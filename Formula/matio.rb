29ca2769d99efa73e51fae28d15baf97058278eb
Sascha Kratky <skratky@gmail.com>
2018-03-21 21:38:25 +0100

    fix matio

--- Formula/matio.rb
+++ Formula/matio.rb
@@ -2,19 +2,16 @@ require 'formula'
 
 class Matio < Formula
   homepage 'https://matio.sourceforge.io/'
-  url 'https://downloads.sourceforge.net/project/matio/matio/1.5.12/matio-1.5.12.7z'
-  sha256 '7228c9b976b932ff19fb3ed57ad0a87a926617734bd117f939f2de3f9cd2e7b0'
+  url 'https://downloads.sourceforge.net/project/matio/matio/1.5.12/matio-1.5.12.tar.gz'
+  sha256 '8695e380e465056afa5b5e20128935afe7d50e03830f9f7778a72e1e1894d8a9'
 
   depends_on 'hdf5'
 
   def install
-    cd "matio-1.5.12" do
-      system "bash", "configure", "--disable-debug", "--disable-dependency-tracking",
-                            "--prefix=#{prefix}", "--with-hdf5=#{HOMEBREW_PREFIX}",
-                            "--enable-mat73=yes"
-      system "make check"
-      system "make install"
-    end
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}", "--with-hdf5=#{HOMEBREW_PREFIX}",
+                          "--enable-mat73=yes"
+    system "make install"
   end
 
 end
