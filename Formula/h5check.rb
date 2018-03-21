5ec84c7ce7ab328d65f61399beb365e37ff7184c
Sascha Kratky <skratky@gmail.com>
2018-03-21 20:35:05 +0100

    fix h5check URLs

--- Formula/h5check.rb
+++ Formula/h5check.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class H5check < Formula
   desc "validation tool for HDF5 files"
-  homepage "https://www.hdfgroup.org/products/hdf5_tools/h5check.html"
-  url "https://www.hdfgroup.org/ftp/HDF5/tools/h5check/src/h5check-2.0.1.tar.gz"
+  homepage "https://support.hdfgroup.org/products/hdf5_tools/h5check.html"
+  url "https://support.hdfgroup.org/ftp/HDF5/tools/h5check/src/h5check-2.0.1.tar.gz"
   sha256 "72a6c2d19dea09962af0ac97277293b58d2119ea9f04fbfa6a2a7dc89734a7be"
 
   depends_on 'hdf5'
