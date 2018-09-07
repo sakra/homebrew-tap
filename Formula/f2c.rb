7535be3a83e0dc85131f9b0161255ba860ce2ce7
Sascha Kratky <skratky@gmail.com>
2018-09-07 15:56:57 +0200

    add f2c

---
+++ Formula/f2c.rb
@@ -0,0 +1,35 @@
+class F2c < Formula
+  desc "Fortran-to-C Converter"
+  homepage "http://www.netlib.org/f2c/"
+  url "http://www.netlib.org/f2c/src.tgz"
+  version "201601021"
+  sha256 "7054ff0f6b3718586911521a368fa89976c33d18add2403d811446ec73a16d2e"
+
+  resource "lib" do
+    url "http://www.netlib.org/f2c/libf2c.zip"
+    sha256 "ca404070e9ce0a9aaa6a71fc7d5489d014ade952c5d6de7efb88de8e24f2e8e0"
+  end
+
+  resource "manual" do
+    url "http://www.netlib.org/f2c/f2c.pdf"
+    sha256 "816cdbfd20ce3695be0eb976648714b6fe496785bb8026c6b8911712764d57c7"
+  end
+
+  def install
+    cp 'makefile.u', 'makefile'
+    system "make"
+    bin.install 'f2c'
+    man1.install 'f2c.1'
+    resource("lib").stage do
+      cp 'makefile.u', 'makefile'
+      make "f2c.h"
+      system "make"
+      include.install("f2c.h")
+      lib.install("libf2c.a")
+    end
+    resource("manual").stage do
+      doc.install 'f2c.pdf'
+    end
+  end
+
+end
