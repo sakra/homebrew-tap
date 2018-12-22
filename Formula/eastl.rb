c3a525e048d60094c5ccb89c2c34e4053d768248
Sascha Kratky <skratky@gmail.com>
2018-12-22 11:54:43 +0100

    EASTL 3.12.04

--- Formula/eastl.rb
+++ Formula/eastl.rb
@@ -1,8 +1,8 @@
 class Eastl < Formula
   desc "Electronic Arts Standard Template Library"
   homepage "https://github.com/electronicarts/EASTL"
-  url "https://github.com/electronicarts/EASTL/archive/3.08.00.tar.gz"
-  sha256 "14f89bdeb0b0ed316408572f7250ce80710f32e42ec4fd1e550f43bb2cf74f90"
+  url "https://github.com/electronicarts/EASTL/archive/3.12.04.tar.gz"
+  sha256 "320aacde350bac832756e300d4ae738a8b20148e52118e18131e673f4bbe9d22"
 
   depends_on "cmake" => :build
 
