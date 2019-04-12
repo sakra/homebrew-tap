82b9f3010dcda3721b4c04ede8ff66bdde2c301b
Sascha Kratky <skratky@gmail.com>
2019-04-12 19:44:37 +0200

    Cpprestsdk v2.10.12

--- Formula/cpprestsdk.rb
+++ Formula/cpprestsdk.rb
@@ -1,8 +1,8 @@
 class Cpprestsdk < Formula
   desc "C++ libraries for cloud-based client-server communication"
   homepage "https://github.com/Microsoft/cpprestsdk"
-  url "https://github.com/Microsoft/cpprestsdk/archive/v2.10.10.tar.gz"
-  sha256 "55e1521fb7b7c9b2f4f2065c5fb47e249227f15299257ee6f1a0c942a4057f4f"
+  url "https://github.com/Microsoft/cpprestsdk/archive/v2.10.12.tar.gz"
+  sha256 "c7c2a5deb4cad036b974e9b7f2ba2e3ae829312894ddfca2fae3a11980fef63e"
   head "https://github.com/Microsoft/cpprestsdk.git", :branch => "development"
 
   depends_on "cmake" => :build
@@ -10,7 +10,7 @@ class Cpprestsdk < Formula
   depends_on "openssl"
 
   def install
-    system "cmake", ".", "-DBUILD_SAMPLES=OFF", "-DBUILD_TESTS=OFF", "-DBUILD_SHARED_LIBS=OFF",
+    system "cmake", ".", "-DBUILD_SAMPLES=OFF", "-DBUILD_TESTS=OFF",
       "-DCPPREST_EXCLUDE_WEBSOCKETS=ON", "-DWERROR=OFF", *std_cmake_args
     system "make", "install"
   end
