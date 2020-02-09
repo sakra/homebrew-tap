479efb13ba640f8bd698cde4ac58eb529edd7790
Sascha Kratky <skratky@gmail.com>
2020-02-09 12:19:21 +0100

    upgrade fxt to 2020.01.29

--- Formula/fxt.rb
+++ Formula/fxt.rb
@@ -1,15 +1,15 @@
 class Fxt < Formula
   desc "Library of low-level algorithms"
   homepage "https://www.jjj.de/fxt/"
-  url "https://www.jjj.de/fxt/fxt-2018.02.15.tar.gz"
-  sha256 "17a4e22556203d0b3f08a0f762d91b4dcbc8ff2b91262c0b85f5de6c6e489e0b"
+  url "https://www.jjj.de/fxt/fxt-2020.01.29.tar.gz"
+  sha256 ""
 
   option :cxx11
 
   def install
     ENV.deparallelize
     ENV.O2
-    ENV.cxx11 if build.cxx11?
+    ENV.cxx11
     lib.mkdir
     include.mkdir
     system "make", "install", "PREFIX=#{prefix}", "CXXFLAGS=#{ENV.cxxflags}", "CXX=#{ENV.cxx}", "FXT_INSTALL=/usr/bin/install"
