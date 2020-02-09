b8e36d5432d1fd162890db8d80ac27f2139062d0
Sascha Kratky <skratky@gmail.com>
2020-02-09 12:20:07 +0100

    upgrade vectorclass to 2.01.00

--- Formula/vectorclass.rb
+++ Formula/vectorclass.rb
@@ -1,21 +1,18 @@
 class Vectorclass < Formula
   desc "C++ vector class library"
   homepage "http://www.agner.org/optimize/#vectorclass"
-  url "http://www.agner.org/optimize/vectorclass.zip"
-  version "2017-Jul-27"
-  sha256 "f9cb70a3e865dd019b58f449d11f90147ce8ba5f2c60410389ec0ead92944b97"
+  url "https://github.com/vectorclass/version2/archive/v2.01.00.tar.gz"
+  version "2.01.00"
+  sha256 "d6e31e0ef619647f1a497a75c9c11c42c4bfb0536f104bc5757d9ca08a7da38e"
 
   depends_on "libtool" => :build
 
   def install
-    system "unzip", "special.zip"
     chmod_R 0644, Dir['*.h']
-    system ENV.cxx, "-c", "ranvec1.cpp", "-o", "ranvec1.o"
-    system ENV.cxx, "-c", "instrset_detect.cpp", "-o", "instrset_detect.o"
-    system "libtool", "-static", "-o", "libvectorclass.a", "instrset_detect.o", "ranvec1.o"
+    system ENV.cxx, "-std=c++14", "-c", "instrset_detect.cpp", "-o", "instrset_detect.o"
+    system "libtool", "-static", "-o", "libvectorclass.a", "instrset_detect.o"
     (lib + name).install "libvectorclass.a"
     (include + name).install Dir['*.h']
-    doc.install 'vectorclass.pdf'
   end
 
 end
