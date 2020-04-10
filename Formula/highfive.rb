fd0f8bafb47d1cbc4ce0488216231a841e07e551
Sascha Kratky <skratky@gmail.com>
2020-04-10 09:11:44 +0200

    HighFive v2.2

--- Formula/highfive.rb
+++ Formula/highfive.rb
@@ -1,8 +1,8 @@
 class Highfive < Formula
   desc "HighFive - Header-only C++ HDF5 interface"
   homepage "https://github.com/BlueBrain/HighFive"
-  url "https://github.com/BlueBrain/HighFive/archive/v1.5.tar.gz"
-  sha256 "f194bda482ab15efa7c577ecc4fb7ee519f6d4bf83470acdb3fb455c8accb407"
+  url "https://github.com/BlueBrain/HighFive/archive/v2.2.tar.gz"
+  sha256 "fe065f2443e38444100b43999a96916e81a0aa7e500cf768d3bf6f8392b8efee"
   
   depends_on "boost"
   depends_on "hdf5"
