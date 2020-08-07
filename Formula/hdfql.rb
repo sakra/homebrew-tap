cac7034157960fd65d3654620c33271eca3ba405
Sascha Kratky <skratky@gmail.com>
2020-08-07 14:47:04 +0200

    upgrade hdfql to 2.2.0

--- Formula/hdfql.rb
+++ Formula/hdfql.rb
@@ -1,23 +1,34 @@
 class Hdfql < Formula
   desc "Hierarchical Data Format query language"
   homepage "http://www.hdfql.com"
-  url "http://www.hdfql.com/releases/1.5.0/HDFql-1.5.0_Darwin64_GCC-4.9.zip"
-  version "1.5.0"
-  sha256 "71ddee86b36c8162cd1cb23bfbbb17d0174b3fa7b4b8dcd8f986ff70df515b60"
+  url "http://www.hdfql.com/releases/2.2.0/HDFql-2.2.0_Darwin64_GCC-4.9.zip"
+  version "2.2.0"
+  sha256 "780bc85484101a28d48b6498692e6ccfaf653eb9135246afce23ade69111a8cb"
 
   depends_on "gcc@4.9"
 
   def install
-    cd "lib" do
-      system "install_name_tool -change /opt/local/lib/libgcc/libgcc_s.1.dylib #{HOMEBREW_PREFIX}/lib/gcc/4.9/libgcc_s.1.dylib libHDFql.dylib"
-      system "install_name_tool -change /opt/local/lib/libgcc/libgomp.1.dylib #{HOMEBREW_PREFIX}/lib/gcc/4.9/libgomp.dylib libHDFql.dylib"
-      lib.install "libHDFql.dylib"
-    end
     cd "bin" do
-      system "install_name_tool -change /opt/local/lib/libgcc/libgcc_s.1.dylib #{HOMEBREW_PREFIX}/lib/gcc/4.9/libgcc_s.1.dylib HDFqlCLI"
-      system "install_name_tool -change ../lib/64/GCC-4.9/libHDFql.dylib #{lib}/libHDFql.dylib HDFqlCLI"
-      bin.install "HDFqlCLI"
+      macho = MachO.open("HDFqlCLI")
+      macho.change_dylib("/opt/local/lib/libgcc/libgcc_s.1.dylib",
+                         "#{HOMEBREW_PREFIX}/lib/gcc/4.9/libgcc_s.1.dylib")
+      macho.write!
+      bin.install Dir["*"]
     end
-    doc.install "doc/HDFqlReferenceManual.pdf"
+    cd "doc" do
+      doc.install Dir["*"]
+    end
+    cd "include" do
+      include.install Dir["*"]
+    end
+    cd "lib" do
+      macho = MachO.open("libHDFql.dylib")
+      macho.change_dylib("/opt/local/lib/libgcc/libgcc_s.1.dylib",
+                         "#{HOMEBREW_PREFIX}/lib/gcc/4.9/libgcc_s.1.dylib")
+      macho.change_dylib("/opt/local/lib/libgcc/libgomp.1.dylib",
+                         "#{HOMEBREW_PREFIX}/lib/gcc/4.9/libgomp.dylib")
+      macho.write!
+      lib.install Dir["*"]
+    end
   end
 end
