1ee66ad96f9dd68eddc0178108157cb5393a9353
Sascha Kratky <skratky@gmail.com>
2021-03-06 15:45:57 +0100

    add formula for half

---
+++ Formula/half.rb
@@ -0,0 +1,15 @@
+class Half < Formula
+  desc "Floating-point library for half-precision floats"
+  homepage "https://half.sourceforge.net/"
+  url "https://sourceforge.net/projects/half/files/half/2.1.0/half-2.1.0.zip"
+  sha256 "ad1788afe0300fa2b02b0d1df128d857f021f92ccf7c8bddd07812685fa07a25"
+  license "MIT"
+
+ depends_on "dos2unix" => :build
+
+  def install
+    system '/usr/bin/find . -type f -exec dos2unix --keepdate "{}" ";"'
+    include.install "include/half.hpp"
+    doc.install "README.txt"
+  end
+end
