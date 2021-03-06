ad92f0e314de9ff14630d1d2415dc4cf6b758524
Sascha Kratky <skratky@gmail.com>
2021-03-06 15:47:05 +0100

    fix formula for half

--- Formula/half.rb
+++ Formula/half.rb
@@ -1,11 +1,11 @@
 class Half < Formula
   desc "Floating-point library for half-precision floats"
-  homepage "https://half.sourceforge.net/"
+  homepage "http://half.sourceforge.net/"
   url "https://sourceforge.net/projects/half/files/half/2.1.0/half-2.1.0.zip"
   sha256 "ad1788afe0300fa2b02b0d1df128d857f021f92ccf7c8bddd07812685fa07a25"
   license "MIT"
 
- depends_on "dos2unix" => :build
+  depends_on "dos2unix" => :build
 
   def install
     system '/usr/bin/find . -type f -exec dos2unix --keepdate "{}" ";"'
